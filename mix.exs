e7c8fdce96b3bf41a4dd18d07c41b31784caa13e
Bachir Çaoui <bachir.caoui@gmail.com>
2017-12-19 19:26:08 +0100

    Update gitignore

--- mix.exs
+++ mix.exs
@@ -14,15 +14,17 @@ defmodule MoneyWatcher.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {MoneyWatcher, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:cowboy, "~> 1.1.0"},
+      {:plug, "~> 1.4.3"},
+      {:bankster, "~> 0.2.2"}
     ]
   end
 end

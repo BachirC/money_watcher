99b251438a3866897d3f1f14b824d4f404ea27d4
Bachir Çaoui <bachir.caoui@gmail.com>
2017-12-19 21:03:14 +0100

    Add router logic

--- lib/money_watcher.ex
+++ lib/money_watcher.ex
@@ -4,7 +4,6 @@ defmodule MoneyWatcher do
   """
 
   use Application
-
   alias MoneyWatcher.FraudChecker
   import Plug.Conn
 
@@ -22,10 +21,28 @@ defmodule MoneyWatcher do
   end
 
   def route("POST", ["accounts", account_id, "debit"], conn) do
-    Plug.Conn.send_resp(conn, :ok, "")
+    %{"amount" => amount} = fetch_query_params(conn).params
+    if validate_params(%{account_id: account_id, amount: amount}) do
+      with pid <- MoneyWatcher.Registry.create(MoneyWatcher.Registry, account_id),
+           :ok <- FraudChecker.add(pid, {String.to_integer(amount), :os.system_time(:milli_seconds)}),
+           :ok <- FraudChecker.check_debit(pid) do
+             Plug.Conn.send_resp(conn, :ok, "")
+           end
+    else
+      Plug.Conn.send_resp(conn, :ok, "Invalid params")
+    end
   end
 
   def route(_, _, conn) do
     Plug.Conn.send_resp(conn, :ok, "")
+  end
+
+  defp validate_params(params) do
+    with {amount, ""} <- Integer.parse(params.amount),
+         true <- Bankster.iban_valid?(params.account_id) && amount > 0 do
+           true
+    else
+      _err -> false
+    end
   end
 end
--- test/money_watcher_test.exs
+++ test/money_watcher_test.exs
@@ -1,3 +1,32 @@
 defmodule MoneyWatcherTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  test "/accounts/:account_id/debit" do
+    conn = conn(:post, "/accounts/DE89370400440532013000/debit", %{"amount" => "1000"})
+    conn = MoneyWatcher.call(conn, [])
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
+
+  test "with invalid account_id" do
+    conn = conn(:post, "/accounts/INVALID_IBAN/debit", %{"amount" => "1000"})
+    conn = MoneyWatcher.call(conn, [])
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
+
+  test "with invalid amount" do
+    conn = conn(:post, "/accounts/INVALID_IBAN/debit", %{"amount" => "-1000"})
+    conn = MoneyWatcher.call(conn, [])
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
+
+  test "invalid url" do
+    conn = conn(:post, "/non_existant")
+    conn = MoneyWatcher.call(conn, [])
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
 end
